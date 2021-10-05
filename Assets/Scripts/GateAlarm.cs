12ca8391ed80acfbaa259a50554e868cfe478c8b
OrAmram100 <or.amram@s.afeka.ac.il>
2021-10-05 16:53:51 +0300

    //added sounds

---
+++ Assets/Scripts/GateAlarm.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateAlarm : MonoBehaviour
+{
+    private AudioSource alarm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        alarm = GetComponent<AudioSource>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+            alarm.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerMotion.cs
+++ Assets/Scripts/PlayerMotion.cs
@@ -2,30 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class PlayerMotion : MonoBehaviour
 {
     private float speed = 40, angularSpeed = 45;
     private CharacterController controller;
     private float rotationAboutY = 0, rotationAboutX = 0;
-    public GameObject camera; // publics must be initialized in Unity
+    public GameObject camera;
     private AudioSource stepSound;
+    
+
+    
+
 
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
-        rotationAboutY = transform.localEulerAngles.y;
+        rotationAboutY += transform.localEulerAngles.y;
         stepSound = GetComponent<AudioSource>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
+
+
+
+
+
+
+
+
 
     // Update is called once per frame
     void Update()
     {
         float dx, dy = -1/*kind of a gravity*/, dz;
+
         // rotation about Y
         rotationAboutY += Input.GetAxis("Mouse X") * angularSpeed * Time.deltaTime;
-        //       camera.transform.localEulerAngles = new Vector3(0, rotationAboutY, 0);
         transform.localEulerAngles = new Vector3(0, rotationAboutY, 0);
 
         // rotation about X
@@ -41,21 +56,15 @@ public class PlayerMotion : MonoBehaviour
         Vector3 motion = new Vector3(dx, dy, dz); // in Local coordinates
         motion = transform.TransformDirection(motion);// change to Global coordinates
         controller.Move(motion);//in Global coordinates
-
-        // if A/W/S/D key was pressed
         if (dz < -0.1 || dz > 0.1 || dx < -0.1 || dx > 0.1)
         {
+
             if (!stepSound.isPlaying)
                 stepSound.Play();
-
-           
         }
+        
+        
 
-        // simple motion
-        //        transform.Translate(new Vector3(dx, dy, dz));
-        // simple motion forward
-        //      transform.Translate(new Vector3(0, 0, 0.1f));
-        // simple motion to the left
-        //      transform.Translate(new Vector3(-0.1f, 0, 0));
+
     }
 }
