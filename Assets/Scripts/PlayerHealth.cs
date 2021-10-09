294eef8afa7eaeb3832b755da237fe7bafbf6e94
OrAmram100 <or.amram@s.afeka.ac.il>
2021-10-09 16:51:55 +0300

    added health slider

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public static PlayerHealth singelton;
     public float currentHealth;
     public float maxHealth = 100f;
+    public Slider healthSlider;
+    public Text healthCounter;
     public bool isDead = false;
 
     private void Awake()
@@ -17,32 +20,41 @@ public class PlayerHealth : MonoBehaviour
     void Start()
     {
         currentHealth = maxHealth;
+        healthSlider.value = maxHealth;
+        UpdateHealthCounter();
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (currentHealth < 0)
-        {
-            currentHealth = 0;
-        }
-    }
+
+
+
     public void DamagePlayer(float damage)
     {
         if (currentHealth > 0)
         {
-            currentHealth -= damage;
-        }
-        else
-        {
-            Dead();
+            if (damage >= currentHealth)
+            {
+                Dead();
+            }
+            else
+            {
+                currentHealth -= damage;
+                healthSlider.value -= damage;
+            }
+            UpdateHealthCounter();
         }
     }
     void Dead()
     {
         currentHealth = 0;
         isDead = true;
+        healthSlider.value = 0;
+        UpdateHealthCounter();
         Debug.Log("Player is dead");
 
+    }
+    private void UpdateHealthCounter()
+    {
+        healthCounter.text = currentHealth.ToString();
     }
 }
